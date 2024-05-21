3de0708945e79a9658298e81c748437740592ba5
Tamas Cservenak <tamas@cservenak.net>
2024-05-21 15:03:31 +0200

    [MWRAPPER-140] only-script type does not honor MAVEN_USER_HOME
    User provided patch
    
    ---
    
    https://issues.apache.org/jira/browse/MWRAPPER-140

--- maven-wrapper-distribution/src/resources/only-mvnw.cmd
+++ maven-wrapper-distribution/src/resources/only-mvnw.cmd
@@ -79,6 +79,9 @@ if ($env:MVNW_REPOURL) {
 $distributionUrlName = $distributionUrl -replace '^.*/',''
 $distributionUrlNameMain = $distributionUrlName -replace '\.[^.]*$','' -replace '-bin$',''
 $MAVEN_HOME_PARENT = "$HOME/.m2/wrapper/dists/$distributionUrlNameMain"
+if ($env:MAVEN_USER_HOME) {
+  $MAVEN_HOME_PARENT = "$env:MAVEN_USER_HOME/wrapper/dists/$distributionUrlNameMain"
+}
 $MAVEN_HOME_NAME = ([System.Security.Cryptography.MD5]::Create().ComputeHash([byte[]][char[]]$distributionUrl) | ForEach-Object {$_.ToString("x2")}) -join ''
 $MAVEN_HOME = "$MAVEN_HOME_PARENT/$MAVEN_HOME_NAME"
 
