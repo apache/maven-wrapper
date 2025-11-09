7443d4eebc98734709481f7c5140d027566da310
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2025-11-09 15:35:17 -0600

    Allow m2 path to be hidden on Windows

--- maven-wrapper-distribution/src/resources/only-mvnw.cmd
+++ maven-wrapper-distribution/src/resources/only-mvnw.cmd
@@ -89,10 +89,10 @@ if (-not (Test-Path -Path $MAVEN_M2_PATH)) {
 }
 
 $MAVEN_WRAPPER_DISTS = $null
-if ((Get-Item $MAVEN_M2_PATH).Target[0] -eq $null) {
+if ((Get-Item -Path $MAVEN_M2_PATH -Force).Target[0] -eq $null) {
   $MAVEN_WRAPPER_DISTS = "$MAVEN_M2_PATH/wrapper/dists"
 } else {
-  $MAVEN_WRAPPER_DISTS = (Get-Item $MAVEN_M2_PATH).Target[0] + "/wrapper/dists"
+  $MAVEN_WRAPPER_DISTS = (Get-Item -Path $MAVEN_M2_PATH -Force).Target[0] + "/wrapper/dists"
 }
 
 $MAVEN_HOME_PARENT = "$MAVEN_WRAPPER_DISTS/$distributionUrlNameMain"
